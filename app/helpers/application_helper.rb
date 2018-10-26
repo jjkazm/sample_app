e846613c3a09162e6014575b2ef2d918c283a44a
jjkazm <jjkazm@gmail.com>
2018-10-26 23:26:37 +0200

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(title)
+    generic = "Ruby on Rails Tutorial Sample App"
+    if title.empty?
+      generic
+    else
+      title + " | " + generic
+    end
+  end
+
 end
