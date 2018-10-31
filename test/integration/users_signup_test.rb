80fa8f670bff61c2bca6c568e64ea9c646bb3a14
jjkazm <jjkazm@gmail.com>
2018-10-31 18:05:25 +0100

    Add errors partial

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test 'not posting invalid user' do
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: "",
+                                        email: "invalid",
+                                        password: "",
+                                        password_confirmation: "tarara"}}
+    end
+    assert_template 'users/new'
+    assert_select 'div.alert.alert-danger'
+    assert_select 'div#error_explanation'
+    assert_select 'form[action="/signup"]'
+  end
+
+  test 'post valid user' do
+    assert_difference 'User.count' do
+      post users_path, params: { user: { name: "validname",
+                                        email: 'email@valid.com',
+                                        password: "haslo1",
+                                        password_confirmation: "haslo1"}}
+    end
+  end
+end
