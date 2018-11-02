1e9d9d82d56a817472a06c546879d939c3ef25b1
jjkazm <jjkazm@gmail.com>
2018-11-02 08:53:14 +0100

    Add methods for user session in session_helper.rb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,16 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+
+  # Returns logged in user (if any)
+  def current_user
+    if session[:user_id]
+      @current_user =|| User.find_by(id: session[:user_id])
+    end
+
+  # Returns true if user is logged in, otherwise false
+  def logged_in?
+    !!current_user
+  end
 end
