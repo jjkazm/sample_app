8a2c97b78ab3943a5dff9e72d3f5139ff1415ba8
jjkazm <jjkazm@gmail.com>
2018-11-04 23:58:10 +0100

    Add remember_digest to user model and hash it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save {self.email = email.downcase}
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -8,12 +9,27 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
 
+  class << self
+    # Returns hash digest for the given password
+    def digest(password)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(password, cost: cost)
+    end
 
-  # Returns hash digest for the given password
-  def User.digest(password)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(password, cost: cost)
+    # Returns a random token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+
+  # Associates a remember token with the user and saves the corresponding remember
+  # digest to the database
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+
   end
 
 
