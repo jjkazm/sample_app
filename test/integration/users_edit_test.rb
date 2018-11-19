a927304061a61e18ebc3cf76bb2deb144511bb06
jjkazm <jjkazm@gmail.com>
2018-11-20 00:13:22 +0100

    Test successful edit of users profile

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -9,9 +9,24 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test 'fails with incorrect input' do
     get edit_user_path @user
     assert_template 'users/edit'
-    patch user_path, params: {user: {name: "Kuba", email: "", password: "",
+    patch user_path, params: {user: {name: "", email: "@@@", password: "",
                                           password_confirmation: ""}}
-    assert_select "div", {class: "alert alert-danger"}
     assert_template 'users/edit'
+    assert_select "div.alert.alert-danger", /This form can't be posted due to 2 errors/
   end
+
+  test 'is succesful with valid input' do
+    get edit_user_path @user
+    assert_template 'users/edit'
+    patch user_path, params: {user: {name: "Kuba", email: "kuba@wp.pl", password: "",
+                                          password_confirmation: ""}}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, "Kuba"
+    assert_equal @user.email, "kuba@wp.pl"
+
+
+  end
+
 end
