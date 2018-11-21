8cb319b395d0713e910e8a8e0fd74fae5a2151fd
jjkazm <jjkazm@gmail.com>
2018-11-21 17:21:18 +0100

    Test delete user action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,7 +4,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
-    @user = users(:jurek)
+    @admin = users(:jurek)
+    @user = users(:agatka)
+    @user2 = users(:user_1)
+
   end
   test "should get new" do
     get signup_path
@@ -24,6 +27,29 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                 admin: true}}
     @user.reload
     assert_not @user.admin?
+  end
+
+  test 'not logged visitor can not delete users and is redirected to login page' do
+    assert_no_difference 'User.count' do
+      delete user_path(@user2)
+    end
+    assert_redirected_to login_path
+  end
+
+  test 'non-admin user can not delete users and ir redirected to home page' do
+    log_in_as @user
+    assert_no_difference 'User.count' do
+      delete user_path(@user2)
+    end
+    assert_redirected_to root_path
+  end
+
+  test 'admin can succesfully delete users' do
+    log_in_as @admin
+    assert_difference 'User.count', -1 do
+      delete user_path(@user2)
+    end
+    assert_redirected_to users_path
   end
 
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,15 +6,25 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   # end
 
   def setup
-    @user = users(:jurek)
+    @admin = users(:jurek)
+    @user = users(:agatka)
   end
 
-  test "index including pagination" do
-    log_in_as @user
+  test "index including pagination when logged as user" do
+    log_in_as @admin
     get users_path
     assert_select 'div.pagination', count: 2
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user.admin == true
+        assert_select 'a[href=?]', user_path(user), method: :delete
+      end
     end
+  end
+
+  test "index for non-admins" do
+    log_in_as @user
+    get users_path
+      assert_select 'a', text: "Delete", count: 0
   end
 end
