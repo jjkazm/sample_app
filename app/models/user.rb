b18fa950070df08a6642934c8caee271f43a3420
jjkazm <jjkazm@gmail.com>
2018-11-28 13:41:42 +0100

    Test Micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
+
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
@@ -9,6 +10,8 @@ class User < ApplicationRecord
             uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+
+  has_many :microposts
 
   class << self
     # Returns hash digest for the given password
---
+++ test/models/micropost_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:jurek)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
+  end
+
+  test 'should be valid' do
+    assert @micropost.valid?
+  end
+
+  test 'micropost need to have user id' do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test 'micropost should have contet' do
+    @micropost.content = ""
+    assert_not @micropost.valid?
+  end
+
+  test "microposts content can't be longer than 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
+
+end
