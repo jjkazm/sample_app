1ff14ae773e3335ee2b1233a9b1b1945ef3ec8fb
jjkazm <jjkazm@gmail.com>
2018-11-30 15:00:48 +0100

    Test interface and deleting of microposts

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -4,6 +4,8 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @micropost = microposts(:apple)
+    @user1 = users(:jurek)
+    @user2 = users(:agatka)
   end
 
   test "should redirect create if not logged in" do
@@ -18,5 +20,21 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
       delete micropost_path (@micropost)
     end
     assert_redirected_to login_path
+  end
+
+  test "user can't delete posts of other users" do
+    log_in_as @user2
+    assert_no_difference "Micropost.count" do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to root_url
+  end
+
+  test 'user can delete his own posts' do
+    log_in_as @user1
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to root_url
   end
 end
---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user1 = users(:jurek)
+    @user2 = users(:agatka)
+    @user3 = users(:user_1)
+  end
+
+  test 'micropost interface' do
+    log_in_as @user1
+    get root_path
+    assert_select 'div.pagination'
+    # Unsuccesfull post
+    assert_no_difference "Micropost.count" do
+      post microposts_path, params: { micropost: { content: "" }}
+    end
+    assert_template 'static_pages/home'
+    assert_select 'div#error_explanation'
+    #succesfull post
+    assert_difference "Micropost.count", 1 do
+      post microposts_path, params: { micropost: { content: "new test post" }}
+    end
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select 'span.content', text: "new test post"
+    # Delete post
+    assert_difference "Micropost.count", -1 do
+      delete micropost_path(@user1.microposts.first)
+    end
+    # No delete links for other user's profile
+    get user_path (@user2)
+    assert_match @user2.name, response.body
+    assert_select 'a', text: 'Delete', count: 0
+  end
+
+  test 'micropost sidebar count' do
+    log_in_as @user1
+    get root_path
+    assert_match "#{@user1.microposts.count} microposts", response.body
+    # User with zero posts
+    log_in_as @user3
+    get root_path
+    assert_match "0 microposts", response.body
+    @user3.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match @user3.microposts.first.content, response.body
+  end
+end
