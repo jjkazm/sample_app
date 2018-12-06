943a904ec72e07ab07c158acc3836f9d2bd230a4
jjkazm <jjkazm@gmail.com>
2018-12-06 16:20:24 +0100

    Add followers and followees actions and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, except:[:new, :create, :index]
-  before_action :logged_in_user, only:[:edit, :update, :index, :destroy]
+  before_action :logged_in_user, only:[:edit, :update, :index, :destroy, :following, :followers]
   before_action :correct_user, only:[:edit, :update]
   before_action :is_admin, only: [:destroy]
 
@@ -50,6 +50,18 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  def following
+    @title = "Following"
+    @users = @user.followees.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -59,7 +71,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-  
+
 
     # Checks if user is the same as user on which some action has to be performed
     def correct_user
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -14,6 +14,9 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'h1', text: @user.name
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination', count: 1
+    assert_select '.stats'
+    assert_match @user.followers.count.to_s, response.body
+    assert_match @user.followees.count.to_s, response.body
     @user.microposts.paginate(page:1).each do |post|
       assert_match post.content, response.body
     end
